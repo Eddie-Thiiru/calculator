acfa7e3a9af74324d8d6c951c7854dd45f453c94
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-01-05 14:14:13 +0300

    Add a subtraction function

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function getSum() {
-    const x = parseInt(prompt("Input first number", 0));
-    const y = parseInt(prompt("Input Second number", 0));
+    const x = 15;
+    const y = 10;
     return x + y;
 }
 const addition = getSum();
 console.log(addition);
 
+function getSubtraction() {
+    const x = 20;
+    const y = 8;
+    return x - y;
+}
+const subtraction = getSubtraction();
+console.log(subtraction);
