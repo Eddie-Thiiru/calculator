47fb452317e8e678998ff1d9f9f9fc2f741cc50c
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-01-05 14:07:38 +0300

    Add an addition function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # calculator
+
+From The Odin Project's curriculum.
+A Project to create a calculator using JavaScript, HTML, and CSS.
+
---
+++ script.js
@@ -0,0 +1,8 @@
+function getSum() {
+    const x = parseInt(prompt("Input first number", 0));
+    const y = parseInt(prompt("Input Second number", 0));
+    return x + y;
+}
+const addition = getSum();
+console.log(addition);
+
