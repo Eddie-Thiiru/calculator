48aba4172bc0611757587deb343c6ccb79fec280
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-01-09 16:59:06 +0300

    Add a function that populates the display when a button is clicked

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const initializer = document.querySelector(".operateButton");
+
+let textContent = "";
+let array = [];
+let stringNum = "";
+
 function getSum() {
-    const x = 15;
-    const y = 10;
     return x + y;
 }
-const add = getSum();
-console.log(add);
 
 function getSubtraction() {
-    const x = 20;
-    const y = 8;
     return x - y;
 }
-const subtract = getSubtraction();
-console.log(subtract);
 
 function getMultiplication() {
-    const x = 10;
-    const y = 8;
     return x * y;
 }
-const multiply = getMultiplication();
-console.log(multiply);
+
+function getDivision() {
+    return x / y;
+}
+
+function displayNumber(e) {
+    const num = e.target.dataset.num;
+    display.textContent += num;
+    stringNum += num; 
+}
+
+function displayOperator(e) {
+    const operator = e.target.dataset.operator;
+    display.textContent += operator;
+    array.push(stringNum);
+    array.push(operator);
+    stringNum = "";
+
+    console.log(stringNum);
+    console.log(array);
+}
+
+function operate(e) {
+    display.textContent = "";
+
+    // if (operator === "+" && operator === "=") {
+    //     console.log(getSum());
+    // } else if (operator === "-" && operator === "=") {
+    //     console.log(getSubtraction());
+    // } else if (operator === "*" && operator === "=") {
+    //     console.log(getMultiplication());
+    // } else if (operator === "/" && operator === "=") {
+    //     console.log(divide = getDivision());
+    // }
+}
+
+numbers.forEach(number => number.addEventListener("click", displayNumber));
+operators.forEach(operator => operator.addEventListener("click", displayOperator));
+initializer.addEventListener("click", operate);
